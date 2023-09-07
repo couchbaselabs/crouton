390b1dd4214de03bb0d98a03f0aa468937e9df78
Jens Alfke <jens@couchbase.com>
2023-09-07 13:05:10 -0700

    Added missing <includes> and license comments

--- include/HTTPHandler.hh
+++ include/HTTPHandler.hh
@@ -1,7 +1,19 @@
 //
 // HTTPHandler.hh
 //
-// 
+// Copyright 2023-Present Couchbase, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #pragma once
@@ -9,7 +21,13 @@
 #include "IStream.hh"
 #include "ISocket.hh"
 #include "Task.hh"
+#include <functional>
+#include <memory>
+#include <optional>
 #include <regex>
+#include <string_view>
+#include <tuple>
+#include <vector>
 
 namespace crouton {
 
