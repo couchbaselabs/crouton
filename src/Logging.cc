ea65ed508bb4ba6f7b103e4623d0d4a827f41137
Jens Alfke <jens@mooseyard.com>
2023-09-26 15:06:05 -0700

    Added missing copyright / license comments

--- src/Logging.cc
+++ src/Logging.cc
@@ -1,7 +1,7 @@
 //
 // Logging.cc
 //
-// 
+// Copyright 2023-Present Couchbase, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
